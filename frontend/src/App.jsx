eb9f49de15b2ddefd71e521f5c4ff250d5d1f282
megha prakasah <meghatp12345@gmail.com>
2024-07-13 14:46:12 +0530

    cart oder details are submited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false)
+
   return (
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:null}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
 
         <Route path='/' element={<Home/>}></Route>
@@ -17,6 +24,9 @@ const App = () => {
 
       </Routes>
     </div>
+    <Footer/>
+    </>
+    
   )
 }
 
--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,19 +1,51 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
-const StoreContextProvider = (props) =>{
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
 
+  //add items to the cart page
 
-    const contextValue = {
-       food_list 
-
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
-    return (
-        <StoreContext.Provider value={contextValue}>
-            {props.children}
-        </StoreContext.Provider>
-    )
-}
-export default StoreContextProvider
+  };
+
+  //remove items from the cart
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  // calculate the total cart amount of products
+  const getTotalCartAmount = (itemId) => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item]>0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+      totalAmount += itemInfo.price * cartItems[item];
+      }      
+    }
+    return totalAmount
+  };
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount
+  };
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+export default StoreContextProvider;
