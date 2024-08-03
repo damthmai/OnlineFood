975a91fce011bd5521b77207ee68bbefeadf1375
megha prakasah <meghatp12345@gmail.com>
2024-08-03 09:31:12 +0530

    stipe added successfully

---
+++ frontend/src/pages/Verify/Verify.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import "./Verify.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import StoreContext from "../../Context/StoreContext";
+import axios from "axios";
+const Verify = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+  const { url } = useContext(StoreContext);
+  const navigate = useNavigate()
+
+ const veryfyPayment = async () => {
+    const response =  await axios.post( url+"/api/order/verify",{success,orderId});
+    if(response.data.success){
+        navigate("/myorders")
+    }
+    else{
+        navigate("/")
+    }
+ }
+
+
+
+
+  return (
+    <div className="verify">
+        <div className="spinner">
+
+        </div>
+    </div>
+    );
+};
+
+export default Verify;
