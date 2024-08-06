5d715ae4b874aa393775444a8b1aa21f90a189d0
megha prakasah <meghatp12345@gmail.com>
2024-08-06 15:23:02 +0530

    admin - order processing

--- admin/src/pages/Orders/Order.jsx
+++ admin/src/pages/Orders/Order.jsx
@@ -1,38 +1,77 @@
-import React from 'react'
-import './Order.css'
-import { useState } from 'react'
-import axios from "axios"
-import {toast} from "react-toastify"
-import { useEffect } from 'react'
+import React from "react";
+import "./Order.css";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
+import { assets } from "../../assets/assets";
 
 const Order = () => {
+  const [orders, setOrders] = useState([]);
+  const url = `http://localhost:4000`;
 
-  const [orders,setOrders] = useState([])
-   const url = `http://localhost:4000`
-  
-  const fetchAllOrders = async () =>{
-    const response = await axios.get(url+"/api/order/list")
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + "/api/order/list");
     if (response.data.success) {
       setOrders(response.data.data);
       console.log(response.data.data);
+    } else {
+      toast.error("Error");
+      console.error(
+        "Fetch orders failed:",
+        response.data && response.data.message
+      );
     }
-    else{
-      toast.error("Error")
-      console.error("Fetch orders failed:", response.data && response.data.message);
-    }
-  }
+  };
 
- 
-
-  useEffect(()=>{
+  useEffect(() => {
     fetchAllOrders();
-  },[])
+  }, []);
 
   return (
-    <div>
-     order 
-    </div>
-  )
-}
+    <div className="order add">
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div key={index} className="order-item">
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className="order-item-food">
+                {order.items.map((item,index)=>{
+                  if (index === order.items.length-1) {
+                    return item.name + " X " + item.quantity;
+                  }
+                  else{
+                    return item.name + " X " + item.quantity + ", ";
 
-export default Order
+                  }
+                })}
+              </p>
+              <p className="order-item-name">
+                {order.address.firstName+ " " +order.address.lastName }
+              </p>
+              <div className="order-item-address">
+                <p>{order.address.street+ ","}</p>
+                <p>{order.address.city+ ", "+order.address.state+", "+order.address.country+", "+order.address.zipcode}</p>
+              </div>
+              <p className="order-item-phone">
+                {order.address.phone}
+              </p>
+            </div>
+            <p>Items : {order.items.length}</p>
+            <p>${order.amount}</p>
+            <select name="" id="">
+              
+                <option value="Food Processing">Food Processing</option>
+                <option value="Out for delivery">Out for delivery</option>
+                <option value="Delivered">Delivered</option>
+              
+            </select>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Order;
